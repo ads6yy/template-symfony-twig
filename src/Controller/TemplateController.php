21e6008ae02dd6acc837b7c2a64b4fccaada4f7f
adsy <asimon@ipsosenso.com>
2025-12-21 20:49:09 +0100

    monolog construct dependency injection

--- src/Controller/TemplateController.php
+++ src/Controller/TemplateController.php
@@ -2,15 +2,28 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TemplateController extends AbstractController
 {
+    public function __construct(
+        protected LoggerInterface $logger,
+    )
+    {
+    }
+
     #[Route('/', name: 'app_template')]
     public function index(): Response
     {
+        $this->logger->info("app_template - start");
+
+        // todo
+
+        $this->logger->info("app_template - end");
+
         return $this->render('template/index.html.twig', [
             'controller_name' => 'TemplateController',
         ]);
