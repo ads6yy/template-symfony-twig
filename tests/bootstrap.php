e4b7ffe5058cde339c8ade253d85d3b2f255cfd3
adsy <asimon@ipsosenso.com>
2026-01-25 18:56:16 +0100

    phpunit tests: copilot review suggestions

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,6 @@ use Symfony\Component\Dotenv\Dotenv;
 require dirname(__DIR__).'/vendor/autoload.php';
 
 new Dotenv()->bootEnv(dirname(__DIR__).'/.env');
-if ($_SERVER['APP_DEBUG']) {
+if ($_SERVER['APP_DEBUG'] ?? false) {
     umask(0000);
 }
